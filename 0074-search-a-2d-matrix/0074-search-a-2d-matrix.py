2c7bd7483ebf50c0715ea3ac6d08eef6f5c7c8e8
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-02-05 12:36:25 +0545

    Time: 35 ms (98.47%), Space: 17.2 MB (62.01%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,23 +1,25 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        colL, colR = 0, len(matrix) - 1
-        colIndex = 0
-        while colL <= colR:
-            mid = (colL + colR) // 2
-            if matrix[mid][0] <= target <= matrix[mid][-1]:
-                colIndex = mid
-                break
-            elif target < matrix[mid][0]:
-                colR = mid - 1
-            else:
-                colL = mid + 1
-        rowL, rowR = 0, len(matrix[colIndex]) - 1
-        while rowL <= rowR:
-            mid = (rowL + rowR) // 2
-            if target == matrix[colIndex][mid]:
+        l, r = 0, len(matrix) - 1
+        group = 0
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[mid][0] == target or target == matrix[mid][-1]:
                 return True
-            elif target < matrix[colIndex][mid]:
-                rowR = mid - 1
+            elif matrix[mid][0] <= target <= matrix[mid][-1]:
+                group = mid
+                break
+            elif target > matrix[mid][-1]:
+                l = mid + 1
             else:
-                rowL = mid + 1
+                r = mid - 1
+        l, r = 0, len(matrix[group]) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[group][mid] == target:
+                return True
+            elif target < matrix[group][mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
         return False
