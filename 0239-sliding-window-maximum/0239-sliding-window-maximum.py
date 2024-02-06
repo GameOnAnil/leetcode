fc356dedfee174ad9d1ba09dab7eeb168fdfb667
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-02-06 19:23:27 +0545

    Time: 1119 ms (66.24%), Space: 32.3 MB (92.38%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -4,11 +4,14 @@ class Solution:
         res = []
         q = collections.deque()
         while r < len(nums):
+            # Remove all item less than current from queue
             while q and nums[q[-1]] < nums[r]:
                 q.pop()
             q.append(r)
+            # Remove item outside of the window
             if l > q[0]:
                 q.popleft()
+            # When r hits the window end add to output
             if (r + 1) >= k:
                 res.append(nums[q[0]])
                 l += 1
