b3b16e27be93c46810cad9a1631c7c6270139a65
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-03-01 16:26:52 +0545

    Time: 164 ms (59.58%), Space: 18.5 MB (68.03%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize:
+            return False
+        count = defaultdict(int)
+        for i in hand:
+            count[i] += 1
+        minHeap = list(count.keys())
+        heapq.heapify(minHeap)
+        while minHeap:
+            first = minHeap[0]
+            for i in range(first, first + groupSize):
+                if i not in count:
+                    return False
+                count[i] -= 1
+                if count[i] == 0:
+                    if i != minHeap[0]:
+                        return False
+                    heapq.heappop(minHeap)
+        return True
