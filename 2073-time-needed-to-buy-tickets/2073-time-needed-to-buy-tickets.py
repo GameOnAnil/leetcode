7185d8ef05e08620b3dbc23bce062ddc2d817d78
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-04-09 08:42:26 +0545

    Time: 56 ms (15.3%), Space: 16.5 MB (51.83%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        dq = deque()
+        for i in range(len(tickets)):
+            dq.append(i)
+        res = 0
+        while (len(tickets) - 1) >= k and tickets[k] > 0:
+            index = dq.popleft()
+            if tickets[index] > 0:
+                tickets[index]=tickets[index]-1
+                res+=1
+            dq.append(index)
+        return res
+
+        
