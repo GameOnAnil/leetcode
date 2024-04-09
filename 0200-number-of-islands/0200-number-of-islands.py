fc6e361bb71f052556fd9d7d58ab16ae7f9ab90d
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-04-09 14:46:47 +0545

    Time: 329 ms (7.24%), Space: 27.2 MB (12.15%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,7 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
         rows = len(grid)
         cols = len(grid[0])
         seen = set()
