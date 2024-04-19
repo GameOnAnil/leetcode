6f36b8065ec1109ab1447966feb7dc7390e5bdc6
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-04-19 08:36:44 +0545

    Time: 305 ms (18.55%), Space: 18.9 MB (83.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,20 +4,19 @@ class Solution:
             return 0
         rows = len(grid)
         cols = len(grid[0])
-        seen = set()
         res = 0
 
         def bfs(r, c):
-            if r not in range(rows) or c not in range(cols) or grid[r][c] == "0" or (r, c) in seen:
+            if r not in range(rows) or c not in range(cols) or grid[r][c] != "1":
                 return
-            seen.add((r, c))
+            grid[r][c] = "#"
             directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
             for i, j in directions:
                 bfs(r + i, c + j)
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in seen:
+                if grid[r][c] == "1":
                     res += 1
                     bfs(r, c)
         return res
