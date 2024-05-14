24a6bdcf312ac000ff3430214f983f8f63d8dddb
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-05-14 16:28:48 +0545

    Time: 286 ms (19.18%), Space: 18.9 MB (81.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        rows = len(grid)
+        rows  = len(grid)
         cols = len(grid[0])
-        res = 0
 
-        def bfs(r, c):
-            if r not in range(rows) or c not in range(cols) or grid[r][c] != "1":
+        def dfs(r,c):
+            if (
+                r not in range(rows)
+                or c not in range(cols)
+                or grid[r][c]!="1"
+            ):
                 return
-            grid[r][c] = "#"
-            directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-            for i, j in directions:
-                bfs(r + i, c + j)
-
+            direction  = ((1,0),(-1,0),(0,1),(0,-1))
+            grid[r][c]="#"
+            for i , j in direction:
+                dfs(r+i,c+j)
+        res = 0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    res += 1
-                    bfs(r, c)
+                    res+=1
+                    dfs(r,c)
         return res
+
+        
