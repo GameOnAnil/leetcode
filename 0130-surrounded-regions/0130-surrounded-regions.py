920c362e78ff0d91d66ef422914be8af4ead803d
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-05-16 18:33:20 +0545

    Time: 126 ms (34.38%), Space: 18 MB (54.92%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,35 +4,36 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         rows, cols = len(board), len(board[0])
-        good = set()
         q = deque()
         def bfs():
             while q:
                 for _ in range(len(q)):
                     r,c = q.popleft()
-                    good.add((r,c))
+                    board[r][c]="#"
                     for x,y in ((1,0),(-1,0),(0,1),(0,-1)):
                         dx,dy = r+x, c+y
-                        if (dx in range(rows) and dy in range(cols) and board[dx][dy] == "O" and (dx,dy) not in good):
+                        if (dx in range(rows) and dy in range(cols) and board[dx][dy] == "O"):
                             q.append((dx,dy))
 
         for i in range(rows):
             if board[i][0]=="O":
                 q.append((i,0))
-                good.add((i,0))
+                board[i][0]="#"
             if board[i][cols-1]=="O":
                 q.append((i,cols-1))
-                good.add((i,cols-1))
+                board[i][cols-1] ="#"
         for j in range(cols):
             if board[0][j]=="O":
                 q.append((0,j))
-                good.add((0,j))
+                board[0][j]="#"
             if board[rows-1][j]=="O":
                 q.append((rows-1,j))
-                good.add((rows-1,j))
+                board[rows-1][j]="#"
         bfs()
         for i in range(rows):
             for j in range(cols):
-                if board[i][j] == "O" and (i,j) not in good:
-                    board[i][j] = "X"                    
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                if board[i][j] == "#":
+                    board[i][j]="O"          
         
