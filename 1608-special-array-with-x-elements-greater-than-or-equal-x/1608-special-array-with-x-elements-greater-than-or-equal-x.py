78bfdd16ed6c41f220715ab275d1e020d4a99701
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-05-27 11:04:10 +0545

    Time: 40 ms (62.87%), Space: 16.5 MB (86.58%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,15 +1,23 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         nums.sort()
-        l = 0
-        for i in range(max(nums) +1 ):
-            l = 0
-            while l < len(nums) and nums[l] < i:
-                l+=1
-            sub = nums[l:]
-            print(i,l)
-            if len(sub) == i:
+        def find_greater(num,i):
+            l,r = 0,len(nums)-1
+            curr = len(nums)
+            while l <= r:
+                mid = (l+r)//2
+                if nums[mid] >= i:
+                    curr = mid
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return curr
+        N = len(nums)
+        for i in range(N + 1):
+            curr = find_greater(nums,i)
+            if i == len(nums[curr:]):
                 return i
         return -1
 
+                    
         
