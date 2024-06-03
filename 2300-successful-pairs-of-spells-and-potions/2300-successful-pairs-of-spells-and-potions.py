707f2d8285e7ea7548b7a70b1e1008bd9d803493
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-06-03 16:14:41 +0545

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,26 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        P = len(potions)
+        S = len(spells)
+        potions.sort()
+        res = [0] * S
+
+        def findPivot(num):
+            l,r = 0, P-1 
+            while l<=r:
+                mid = (l+r)//2
+                if potions[mid]*num == success:
+                    return mid
+                elif potions[mid]*num < success:
+                    l = mid + 1                  
+                else:
+                    r = mid - 1
+            return l
+
+        for i, s in enumerate(spells):
+            pivot = findPivot(s)
+            res[i] = P - pivot
+        return res
+
+
+        
