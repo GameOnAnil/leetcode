dd4b39f8ed9a3b6c226b7dc12544e61982c83ba8
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-06-04 08:37:32 +0545

    Time: 43 ms (15.94%), Space: 16.5 MB (98.44%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,14 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        count = defaultdict(int)
-        for i in s:
-            count[i] += 1
+        count = Counter(s)
+        oddSeen = False
         res = 0
-        oddExists = False
-        for c in count.values():
-            if (c % 2) == 0:
-                res = res + c
+        for k,i in count.items():
+            if i%2==0:
+                res+=i
             else:
-                res = res + c - 1
-                oddExists = True
-        return res + 1 if oddExists else res
+                curr = count.get(k)
+                if curr >0:
+                    oddSeen =True
+                    res+=curr-1
+        if oddSeen:
+            res+=1
+        return res
