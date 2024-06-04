59e05b33113811be7a3e6a0292e10ae8930c1c14
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-06-04 20:25:53 +0545

    Time: 196 ms (50.34%), Space: 30.2 MB (81.59%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -5,19 +5,20 @@ class Solution:
 
         for i in range(1,len(nums)):
             prefix[i] = prefix[i-1] + nums[i]
-
-        res = 10**4 + 1
-        
+        INF = 10**9 + 1
+        res = INF
+        l = 0
+        print(max(prefix))
         for r, p in enumerate(prefix):
             if p < target:
                 continue
-            l = 0
-            while l <= r and (prefix[r] - prefix[l]) >=target:
+            
+            while (prefix[r] - prefix[l]) >=target:
                 l+=1
             # print("VALID",l,r,prefix[r],prefix[l])
             res = min(res,r-l+1)
 
-        return res if res!=(10**4+1) else 0
+        return res if res!=INF else 0
 
 
         
