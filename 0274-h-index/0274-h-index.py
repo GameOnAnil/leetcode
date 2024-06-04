fb5fe3484042389105eb935ecfe9653ac2c298c0
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-06-04 21:04:51 +0545

    Time: 39 ms (69.79%), Space: 17 MB (28.8%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        N = len(citations)
+        if not citations:
+            return 0
+        l,r = 0, N - 1
+        while l <= r:
+            mid = l + (r-l)//2
+            curr = N - mid
+            if citations[mid] == curr:
+                return citations[mid]
+            elif citations[mid] > curr:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return N - l
+        
