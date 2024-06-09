3d4613aa0eb307cc464d87aa215171d7f95752bf
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-06-09 14:09:56 +0545

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,9 +1,22 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        time = [float(target - p) / s for p, s in sorted(zip(position, speed))]
-        res = curr = 0
-        for t in time[::-1]:
-            if t > curr:
-                res += 1
-                curr = t
+        comb = []
+        for p,s in zip(position,speed):
+            comb.append([p,s])
+        comb = sorted(comb,key = lambda x:x[0])
+        print(comb)
+
+        res = 0
+        l = 0
+        while l < len(comb):
+            curr = comb[l]
+            r = l
+            while (r+1) < len(comb) and comb[r+1][1] <= curr[1]:
+                r+=1
+            curr = [curr[0],min(curr[1],comb[r][1])]
+            l = r+1
+            res+=1
+
         return res
+
+        
