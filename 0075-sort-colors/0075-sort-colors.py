fbf5815beb59383ad865a8c17e214508a1fe8520
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-06-12 08:35:36 +0545

    Time: 39 ms (45.53%), Space: 16.4 MB (97.3%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -6,8 +6,6 @@ class Solution:
         bucket = [0]*3
         for n in nums:
             bucket[n]+=1
-        temp = []
-        # print(bucket)
         index = 0
         for i,v in enumerate(bucket):
             for _ in range(v):
