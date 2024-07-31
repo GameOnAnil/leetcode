3c2a233cfd95418d805cffbb40e7cf47acea18c7
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-07-31 21:43:11 +0545

    Time: 48 ms (79.38%), Space: 17.7 MB (30.51%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
@@ -1,26 +1,20 @@
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
-        n = len(books)
-        # dp[i] will store the minimum height of the bookcase containing all
-        # books up to and excluding i
-        dp = [0] * (n + 1)
+        @cache
+        def helper(i):
+            if i == len(books):
+                return 0
+            curr_width = shelfWidth
+            max_height = 0
+            res = float("inf")
+            for j in range(i,len(books)):
+                width, height = books[j]
+                if curr_width < width:
+                    break
+                curr_width -= width
+                max_height = max(max_height,height)
+                res = min(res, helper(j+1) + max_height)
+            return res
 
-        # Base cases
-        dp[0] = 0
-        dp[1] = books[0][1]
-
-        for i in range(2, n + 1):
-            # new shelf built to hold current book
-            remaining_shelf_width = shelfWidth - books[i - 1][0]
-            max_height = books[i - 1][1]
-            dp[i] = books[i - 1][1] + dp[i - 1]
-
-            j = i - 1
-            # calculate the height when previous books are added to new shelf
-            while j > 0 and remaining_shelf_width - books[j - 1][0] >= 0:
-                max_height = max(max_height, books[j - 1][1])
-                remaining_shelf_width -= books[j - 1][0]
-                dp[i] = min(dp[i], max_height + dp[j - 1])
-                j -= 1
-
-        return dp[n]
+        return helper(0)
+        
