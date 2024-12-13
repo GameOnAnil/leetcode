8b8669e7c51d2658af9864a3a1235ab81fdd02dd
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2024-12-13 14:39:49 +0545

    Time: 15 ms (48.03%), Space: 17.1 MB (43.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,11 +2,11 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        seen = defaultdict(int)
+        seen = [0] * 26
         for i in range(len(s)):
-            seen[s[i]]+=1
-            seen[t[i]]-=1
-        for v in seen.values():
+            seen[ord(s[i]) - ord("a")]+=1
+            seen[ord(t[i]) - ord("a")]-=1
+        for v in seen:
             if v!=0:
                 return False
         return True
