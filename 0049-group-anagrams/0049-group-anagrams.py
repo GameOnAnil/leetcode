8c6443d4b629987b745c06bc909c03654764a354
Anil Thapa <67099170+GameOnAnil@users.noreply.github.com>
2025-09-03 16:39:32 +0545

    Time: 15 ms (49.38%), Space: 20.7 MB (62.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         sortedList = []
-        r= {}
-        for i in range(0,len(strs)):
+        r = {}
+        for i in range(0, len(strs)):
             sortedList.append("".join(sorted(strs[i])))
-        for i in range(0,len(sortedList)):
+        for i in range(0, len(sortedList)):
             if not r.__contains__(sortedList[i]):
-                r[sortedList[i]]=[strs[i]]
+                r[sortedList[i]] = [strs[i]]
             else:
                 prev = r.get(sortedList[i])
                 prev.append(strs[i])
-                r[sortedList[i]]=prev
-        return r.values()
-
-        
+                r[sortedList[i]] = prev
+        return list(r.values())
